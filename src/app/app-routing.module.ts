a203d88c92180d634699c01e1bc5d8c0d0b4c1c4
vraj2611 <vraj2611@gmail.com>
2019-02-24 22:01:18 -0300

    ajuste tela
    ajuste da tela e inclusao do botao sobre e inclusao da pagina sobre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'sobre', loadChildren: './sobre/sobre.module#SobrePageModule' }
 ];
 
 @NgModule({
